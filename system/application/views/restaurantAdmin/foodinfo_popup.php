c2bd8221b4c430b4c4afa5c80313e1162c391238
Overmind JIANG <p90eri@gmail.com>
2011-06-07 11:01:03 +0800

    unifying ui

--- system/application/views/restaurantAdmin/foodinfo_popup.php
+++ system/application/views/restaurantAdmin/foodinfo_popup.php
@@ -41,6 +41,116 @@
       wrapper.css('width', '720px');
 
       var item_option_factory = function (item_name, item_dom) {
+        var construct_item_options = function (data) {
+          /* get data from server */
+          var items_fetched = data[item_name] || {};
+
+          /* the table to display cells */
+          var table_dom = $('<table border="1"></table>');
+          item_dom.append(table_dom);
+
+          /* the cells */
+          var table_cells = RMan.popup[item_name + '_cells'] = [];
+
+          /* construct one cell */
+          var construct_cell = function (name, enabled) {
+            var cell_created = $('<span selected="0"><input value="' +
+                                 name + '"></input></span>');
+            var btn = $('<button>enable</button>');
+            var del_this = $('<a>x</a>');
+
+            /* toggle selection */
+            btn.click(function (e) {
+              var par = $($(this).parent());
+              var was_enabled = parseInt(par.attr('selected'));
+              if (was_enabled) {
+                $(this).text('enable');
+                par.attr('selected', '0');
+              } else {
+                $(this).text('disable');
+                par.attr('selected', '1');
+              }
+            });
+
+            del_this.click(function (e) {
+              var par = $($(this).parent());
+              $.each(table_cells, function (key, val) {
+                if ($(val)[0] == par[0]) {
+                  /* got this cell -- delete it
+                     and issue a deletion to the server */
+                  var post_data = {};
+                  var to_del = post_data[item_name] = {};
+                  var that_item_name = par.find('input').val();
+                  to_del[that_item_name] = 1;
+
+                  $.post(del_path + String(food_type), {
+                    delete_data: JSON.stringify(post_data)
+                  }, function (res) {
+                    console.log('deletion succ', res);
+                  });
+                  table_cells[key] = null;
+                }
+              });
+              construct_table();  /* after deletion: resize the table */
+            });
+
+            cell_created.append(btn);
+            cell_created.append(del_this);
+            table_cells.push(cell_created);
+
+            if (enabled) /* set enable */
+              btn.trigger('click');
+          };
+
+          /* called when the table is modified */
+          var construct_table = function () {
+            /* note as before $1.6, $.map only supports iter on arrays */
+            var new_cells = $.map(table_cells, function (item, i) {
+              if (item) return item;
+            });
+
+            /* update origin cells as well as data */
+            table_cells = RMan.popup[item_name + '_cells'] = new_cells;
+
+            /* clean all */
+            table_dom.find('tr').detach();
+
+            /* put on */
+            $.each(table_cells, function (i, item) {
+              var item_col = parseInt(i % 3);
+              if (item_col == 0) {
+                /* starting a new row */
+                tr_dom = $('<tr></tr>');
+                table_dom.append(tr_dom);
+              }
+              var td_dom = $('<td></td>');
+              td_dom.append(item);
+              tr_dom.append(td_dom);
+            });
+
+            /* and add a appender cell at the back */
+            var i = table_cells.length;
+            var item_col = parseInt(i % 3);  /* 3 cols per row */
+            if (item_col == 0) {
+              /* starting a new row */
+              tr_dom = $('<tr></tr>');
+              table_dom.append(tr_dom);
+            }
+            var td_dom = $('<td></td>');
+            var appender = $('<a>add one more</a>');
+            td_dom.append(appender);
+            tr_dom.append(td_dom);
+
+            /* click to append one textfield for input */
+            appender.click(function (e) {
+              construct_cell('', true);
+              construct_table();  /* and re-cons the table */
+            });
+          };
+
+        };
+
+        return construct_item_options;
       };
 
       /* -*- SOUP option ctor -*- */
